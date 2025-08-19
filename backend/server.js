7c51684210ba64b206ace5f78070a866fcb5fd33
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-19 09:09:48 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Health check
+app.get("/", (req, res) => res.send("Arvyax wellnessa Hub API is running"))
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// DB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
