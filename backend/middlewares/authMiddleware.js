fa9e3a2d940618f05f57e21243f5ceb3d19e7e33
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-19 08:49:39 +0530

    auth middleware added

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+    if (!user)
+      return res.status(401).json({ message: "User no longer exists" });
+
+    req.userId = user._id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
